baed25aaf72c849ff0eb24b42fe61973af8465e9
Phil Fluegel <pfluegelcx@gmail.com>
2016-03-14 16:16:27 -0500

    working controller

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,12 @@
+(function() {
+ 	function AlbumCtrl(){
+		this.albumData = angular.copy(albumPicasso);
+		console.log(this.albumData);
+	}
+	
+	angular
+		.module('blocJams')
+		.controller('AlbumCtrl', AlbumCtrl);
+
+	
+ })();

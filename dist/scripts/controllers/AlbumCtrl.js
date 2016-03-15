c55ad0d4b8ee4a242bf7c1320ce6042ba50c35e1
Phil Fluegel <pfluegelcx@gmail.com>
2016-03-15 08:17:29 -0500

    album service

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,11 @@
 (function() {
- 	function AlbumCtrl(){
-		this.albumData = angular.copy(albumPicasso);
-		console.log(this.albumData);
+ 	function AlbumCtrl(Fixtures){
+		this.albumData = Fixtures.getAlbum();
 	}
 	
 	angular
 		.module('blocJams')
-		.controller('AlbumCtrl', AlbumCtrl);
+		.controller('AlbumCtrl', ['Fixtures', AlbumCtrl);
 
 	
  })();

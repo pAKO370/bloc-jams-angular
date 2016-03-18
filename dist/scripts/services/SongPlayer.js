fff84e639775c0ec4479acf1382a46df8fdb6ae1
Phil Fluegel <pfluegelcx@gmail.com>
2016-03-18 07:31:24 -0500

    working

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function(){
-	function SongPlayer(Fixtures) {
+	function SongPlayer($rootScope, Fixtures) {
 		var SongPlayer = {};
 		
 		/** 
@@ -48,10 +48,23 @@
 				formats: ['mp3'],
 				preload: true
 			});
+			
+			currentBuzzObject.bind('timeupdate', function(){
+				$rootScope.$apply(function(){
+					SongPlayer.currentTime = currentBuzzObject.getTime();
+				});
+			});
+			
 			SongPlayer.currentSong = song;
 		};
 		
 		SongPlayer.currentSong = null;
+		
+		/**
+		* @desc Current playback time in seconds of currently playing song
+		*/
+		
+		SongPlayer.currentTime = null;
 		
 		var playSong = function(song){
 		currentBuzzObject.play();
@@ -104,14 +117,19 @@
 			
 			
 		};
-		console.log(currentAlbum);
+		
+		SongPlayer.setCurrentTime = function(time) {
+			if (currentBuzzObject) {
+				currentBuzzObject.setTime(time);
+			}
+		};
 		
 		return SongPlayer;
 	}
 	
 	angular
 		.module('blocJams')
-		.factory('SongPlayer', SongPlayer);
+		.factory('SongPlayer',['$rootScope', 'Fixtures', SongPlayer]);
 	
 	
 })();
